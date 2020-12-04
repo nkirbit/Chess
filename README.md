30edef3c4512e64359174768237065b869057f56
Nick Kirby <nickkirby317@gmail.com>
2020-12-04 15:47:01 -0500

    Updating README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# nkirbit_chess
+# Chess
 Code for a chess game. For fun and learning!
+
+Project started 12/4/20. The rough list of steps I would like to complete are:
+
+1) Define what a piece is, and what a chess board is.
+2) Display the board and pieces to the user. This won't be pretty initially, just enough to see the board state.
+3) Build system to ask the user for move input, figure out of it is a possible move, and perform the move, update the board, and ask for the next move.
+4) Define what a "check" is, and how it restricts moves.
+5) Add in elements such as castling and promotion.
+6) Add in game ending conditions. Checkmate, Stalemate.  Maybe repeated board states?
+7) Once the game structure is complete, work on building a better user interface.
--- pieces.py
+++ pieces.py
@@ -20,18 +20,18 @@ class ChessPiece:
         self.color = color
         self.location = location
         self.possible_moves = self.get_possible_moves()
-        
+
     def move(self, new_location):
         ### All pieces have the ability to move.  Two things happen here:
         ### 1) The location is updated to the new location.
-        ### 2) The possible moves are updated 
+        ### 2) The possible moves are updated
         ###
         ### Whether a move is valid needs to be checked.  Not sure if this
         ### will happen here or in the function managing the game.
         self.location = new_location
         self.possible_moves = self.get_possible_moves()
         return self
-    
+
     def radiate_outwards(self,file_delta,rank_delta):
         ### Extends outward from a location in a particular direction.
         ### file_delta and rank_delta can take on values of -1, 0, or 1.
@@ -41,6 +41,9 @@ class ChessPiece:
         ### is not a valid move!
         ###
         ### This function is used by the Queen, Rook, and Bishop.
+
+        ### Eventually this will likely stop if it hits a piece, but it
+        ### doesn't for now.
         possible_moves = []
         if file_delta == 0 and rank_delta == 0:
             return []
@@ -51,12 +54,12 @@ class ChessPiece:
         ### Each step, we will check if the piece is on the board. This uses
         ### The helper function is_on_board
         while self.is_on_board(file_pos,rank_pos):
-            possible_move = (file_pos,rank_pos)    
+            possible_move = (file_pos,rank_pos)
             possible_moves.append(possible_move)
             file_pos += file_delta
             rank_pos += rank_delta
         return possible_moves
-        
+
     def is_on_board(self,file,rank):
         if file >= 0 and file <= 7 and rank >= 0 and rank <= 7:
             return True
@@ -68,7 +71,7 @@ class King(ChessPiece):
         ChessPiece.__init__(self,color,location)
         self.piece = "K"
         self.has_moved = False
-        
+
     def get_possible_moves(self):
         ### Will not worry about castling for now.  The possible moves are the
         ### 8 squares around the king, provided they are on the board.
@@ -90,7 +93,7 @@ class Queen(ChessPiece):
     def __init__(self,color,location):
         ChessPiece.__init__(self,color,location)
         self.piece = "Q"
-        
+
     def get_possible_moves(self):
         possible_moves = []
         ## Call radiate_outwards for each possible direction.
@@ -98,11 +101,11 @@ class Queen(ChessPiece):
             for rank_delta in [-1,0,1]:
                 possible_moves.extend(self.radiate_outwards(file_delta,rank_delta))
         return possible_moves
-    
+
 class Rook(ChessPiece):
     def __init__(self,color,location):
         ChessPiece.__init__(self,color,location)
-        self.piece = "R"    
+        self.piece = "R"
 
     def get_possible_moves(self):
         possible_moves = []
@@ -112,11 +115,11 @@ class Rook(ChessPiece):
         possible_moves.extend(self.radiate_outwards(1,0))
         possible_moves.extend(self.radiate_outwards(-1,0))
         return possible_moves
-    
+
 class Bishop(ChessPiece):
     def __init__(self,color,location):
         ChessPiece.__init__(self,color,location)
-        self.piece = "B"    
+        self.piece = "B"
 
     def get_possible_moves(self):
         possible_moves = []
@@ -126,11 +129,11 @@ class Bishop(ChessPiece):
         possible_moves.extend(self.radiate_outwards(1,-1))
         possible_moves.extend(self.radiate_outwards(-1,1))
         return possible_moves
-    
+
 class Knight(ChessPiece):
     def __init__(self,color,location):
         ChessPiece.__init__(self,color,location)
-        self.piece = "N" 
+        self.piece = "N"
 
     def get_possible_moves(self):
         file = self.location[0]
@@ -148,15 +151,15 @@ class Knight(ChessPiece):
             for rank_delta in [-1,1]:
                 if self.is_on_board(file+file_delta,rank+rank_delta):
                     possible_move = (file+file_delta,rank+rank_delta)
-                    possible_moves.append(possible_move)                    
+                    possible_moves.append(possible_move)
         return possible_moves
-    
+
 class Pawn(ChessPiece):
     ### Still need to handle promotion. I think that should be handled
     ### by adjusting the move method, but I will leave that for later.
     def __init__(self,color,location):
         ChessPiece.__init__(self,color,location)
-        self.piece = "P"    
+        self.piece = "P"
 
     def get_possible_moves(self):
         ### For now, this will just capture moving forward.
@@ -170,7 +173,7 @@ class Pawn(ChessPiece):
         ### Black and White move in opposite directions, so will
         ### just immediately ask what color the piece is.
         if self.color == "White":
-            ###White 
+            ###White
             if rank == 1:
                 ### rank = 1 is starting space for White. If it is on this
                 ### space, it can move one or two spaces.
